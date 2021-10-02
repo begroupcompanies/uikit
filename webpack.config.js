49125f5b21fc9fe8aaa43df47655ee400ddbf6d3
duxjava <duxjava@gmail.com>
2021-10-02 17:45:48 +0800

    update button style

--- webpack.config.js
+++ webpack.config.js
@@ -23,22 +23,6 @@ module.exports = {
                     // Translates CSS into CommonJS
                     'css-loader',
                     'sass-loader',
-                    {
-                        // Run postcss actions
-                        loader: 'postcss-loader',
-                        options: {
-                            // `postcssOptions` is needed for postcss 8.x;
-                            // if you use postcss 7.x skip the key
-                            postcssOptions: {
-                                // postcss plugins, can be exported to postcss.config.js
-                                plugins: function () {
-                                    return [
-                                        require('autoprefixer')
-                                    ];
-                                }
-                            }
-                        }
-                    },
                 ]
             },
         ],
